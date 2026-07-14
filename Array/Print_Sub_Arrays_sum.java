526be616a7904b485693118fa646a6fd702cd3a1
Gaurav Diware <gauravdiware920@gmail.com>
2026-07-14 23:33:13 +0530

    Day 3
    Finding Max sub_array sum.

--- Array/Print_Sub_Arrays_sum.java
+++ Array/Print_Sub_Arrays_sum.java
@@ -1,30 +1,37 @@
+
+//Brute force approach to print all subarrays of an array and their sum and find Maximum
+
+// Time complexity (n^3) 
+
+
 import java.util.*;
 
-public class Print_Sub_Array {
+
+public class Print_Sub_Arrays_sum{
 
     public static void printSubArrays(int[] numbers) {
-        int totalSubArrays = 0;
+        int curr_sum = 0;
+        int max_sum = Integer.MIN_VALUE;
 
         for (int start = 0; start < numbers.length; start++) {
 
             for (int end = start; end < numbers.length; end++) {
 
-                int sum = 0;
-
-           
+                curr_sum = 0;
                 for (int k = start; k <= end; k++) {
-                    System.out.print(numbers[k] + " ");
-                    sum += numbers[k];
+                    curr_sum += numbers[k];
                 }
 
-                System.out.println(" --> Sum =  " + sum);
-                totalSubArrays++;
+                System.out.println(" sub_Array sum " + curr_sum);
+                if(max_sum < curr_sum){
+                    max_sum = curr_sum;
+                }
+
             }
 
-            System.out.println();
         }
 
-        System.out.println("Total Subarrays = " + totalSubArrays);
+        System.out.println("Maximum subarray sum = " + max_sum);
     }
 
     public static void main(String[] args) {
