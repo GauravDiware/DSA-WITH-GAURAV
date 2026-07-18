8b676559673c3301a601db188085fa43c9467478
Gaurav Diware <gauravdiware920@gmail.com>
2026-07-18 23:13:25 +0530

    Find Second largest

---
+++ Array/SecondLargest.java
@@ -0,0 +1,23 @@
+public class SecondLargest {
+
+    public static int secondLargest(int[] arr) {
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+
+        return (secondLargest == Integer.MIN_VALUE) ? -1 : secondLargest;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 35, 1, 10, 34, 1};
+        System.out.println(secondLargest(arr)); // Output: 34
+    }
+}
